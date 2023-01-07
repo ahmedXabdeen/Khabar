85720fde9137bebe5e8c9d8691a4e3386de91fc4
ahmedXabdeen <wdabdeen98@gmail.com>
2023-01-07 07:05:47 +0200

    Initial Commit

---
+++ Khabar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Khabar
+//
+//  Created by Ahmed Abdeen on 07/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
